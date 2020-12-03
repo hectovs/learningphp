880ad2a65a1889e929be8141fc911dcd9ea69986
Hectovs <htjvans@gmail.com>
2020-12-03 22:45:15 +0000

    end of step 12

--- step10-/index.php
+++ step10-/index.php
@@ -2,11 +2,41 @@
 
 require 'functions.php';
 
-$animals= ['dog', 'cat'];
 
-allowedin(21);
 
-dumpdie('hello', 'big', 'world');
+class Task {
+    public $description; 
+
+    public $completed = false; 
+
+    public function __construct($description)
+    {
+        //automatically triggered on instantiation
+        $this ->description = $description;
+    }
+
+    public function complete()
+    {
+        $this -> completed=true;
+    }
+
+    public function isComplete()
+    {
+        return $this -> completed; 
+    }
+}
+
+$task = new Task('Go to store');
+
+$tasks = [
+    new Task('Go to the store'),
+    new Task('finish this course'),
+    new Task('Do some washing')
+];
+
+
+
+$tasks[0] -> complete();
 
 
 
--- step10-/index.view.php
+++ step10-/index.view.php
@@ -23,7 +23,19 @@
 </head>
 
 <body>
-    <ul> </ul>
+    <ul>
+        <?php foreach ($tasks as $task): ?>
+
+            <li>
+                <?php if($task->completed): ?>
+                    <strike><?= $task->description ?></strike> 
+                <?php else: ?>
+                    <?= $task->description ?>    
+                <?php endif; ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
 </body>
 
 </html>
